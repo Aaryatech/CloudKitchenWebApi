3cf725baaf7221170e1e4fc6c1c6d9f2ea7b652e
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-07-15 10:48:36 +0530

    /Akshay initial

---
+++ src/main/java/com/ats/ckweb/CloudKitchenWebapiApplication.java
@@ -0,0 +1,14 @@
+package com.ats.ckweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudKitchenWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudKitchenWebapiApplication.class, args);
+	}
+	
+
+}
