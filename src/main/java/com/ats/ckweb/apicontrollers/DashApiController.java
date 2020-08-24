dba419eb079231d86743cc42076f26a3067ba804
maddy <maddy@maddy-Inspiron-3541>
2020-08-24 14:50:42 +0530

    //maddy

--- src/main/java/com/ats/ckweb/apicontrollers/DashApiController.java
+++ src/main/java/com/ats/ckweb/apicontrollers/DashApiController.java
@@ -31,4 +31,193 @@ public class DashApiController {
 		}
 		return res;
 	}
+	
+	@RequestMapping(value = { "/getAllStatusFrCount" }, method = RequestMethod.POST)
+	@ResponseBody
+	public List<GetDashPieStatusCnt> getAllStatusFrCount(@RequestParam("fromDate") String fromDate, @RequestParam("toDate") String toDate,
+			@RequestParam("status") String status) {
+		List<GetDashPieStatusCnt> res = new ArrayList<GetDashPieStatusCnt>();
+		try {
+			
+			res = dashRepo.getStatusCntGrpFr(fromDate, toDate, status);
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return res;
+	}
+	
+	
+	
+	
+	
+//	SELECT
+//    t1.fr_id,
+//    t1.fr_name,
+//    COALESCE(t2.status0,0) AS status0,
+//    COALESCE(t3.status1,0) AS status1,
+//    COALESCE(t4.status2,0) AS status2,
+//    COALESCE(t5.status3,0) AS status3,
+//    COALESCE(t6.status4,0) AS status4,
+//    COALESCE(t7.status5,0) AS status5,
+//    COALESCE(t8.status6,0) AS status6,
+//    COALESCE(t9.status7,0) AS status7,
+//    COALESCE(t10.status8,0) AS status8,
+//    COALESCE(t11.status9,0) AS status9
+//FROM
+//    (
+//    SELECT
+//        h.order_id,
+//        h.fr_id,
+//        CONCAT(fr.fr_name, "-", fr.fr_code) AS fr_name,
+//        0 AS status0,
+//        0 AS status1,
+//        0 AS status2,
+//        0 AS status3,
+//        0 AS status4,
+//        0 AS status5,
+//        0 AS status6,
+//        0 AS status7,
+//        0 AS status8,
+//        0 AS status9
+//    FROM
+//        tn_order_header h,
+//        m_franchisee fr
+//    WHERE
+//        h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.fr_id = fr.fr_id AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t1
+//LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status0
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 0 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t2
+//ON
+//    t1.fr_id = t2.fr_id
+// LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status1
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 1 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t3
+//ON
+//    t1.fr_id = t3.fr_id
+//LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status2
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 2 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t4
+//ON
+//    t1.fr_id = t4.fr_id
+//    LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status3
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 3 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t5
+//ON
+//    t1.fr_id = t5.fr_id
+//    
+//    LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status4
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 4 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t6
+//ON
+//    t1.fr_id = t6.fr_id
+//    LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status5
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 5 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t7
+//ON
+//    t1.fr_id = t7.fr_id
+//   LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status6
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 6 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t8
+//ON
+//    t1.fr_id = t8.fr_id
+//   LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status7
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 7 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t9
+//ON
+//    t1.fr_id = t9.fr_id
+//   LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status8
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 8 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t10
+//ON
+//    t1.fr_id = t10.fr_id
+//     LEFT JOIN(
+//    SELECT h.order_id,
+//        h.fr_id,
+//        COUNT(h.order_status) AS status9
+//    FROM
+//        tn_order_header h
+//    WHERE
+//        h.order_status = 9 AND h.delivery_date BETWEEN '2020-08-01' AND '2020-08-29' AND h.del_status = 0
+//    GROUP BY
+//        h.fr_id
+//) t11
+//ON
+//    t1.fr_id = t11.fr_id
+	
 }
--- src/main/java/com/ats/ckweb/report/repo/GetDashPieStatusCntRepo.java
+++ src/main/java/com/ats/ckweb/report/repo/GetDashPieStatusCntRepo.java
@@ -11,27 +11,110 @@ import com.ats.ckweb.report.model.GetDashPieStatusCnt;
 public interface GetDashPieStatusCntRepo extends JpaRepository<GetDashPieStatusCnt, Integer> {
 
 	@Query(value="SELECT\n" + 
-			"	UUID() AS id,\n" + 
-			"    COUNT(order_status) order_status_cnt,\n" + 
-			"    head.order_status,\n" + 
-			"    CASE WHEN head.order_status=0 THEN 'Park Order'\n" + 
-			"      WHEN head.order_status=1 THEN 'Shop Confirmation Pending'\n" + 
-			"       WHEN head.order_status=2 THEN 'Accept'\n" + 
-			"        WHEN head.order_status=3 THEN 'Processing'\n" + 
-			"         WHEN head.order_status=4 THEN 'Delivery Pending'\n" + 
-			"          WHEN head.order_status=5 THEN 'Delivered'\n" + 
-			"           WHEN head.order_status=6 THEN 'Rejected by Shop'\n" + 
-			"            WHEN head.order_status=7 THEN 'Return Order'\n" + 
-			"          	 WHEN head.order_status=8 THEN 'Cancelled Order'\n" + 
-			"           ELSE  'Online Payment Pending'\n" + 
-			"           END AS status_name,\n" + 
-			"    head.ex_int1,\n" + 
-			"    head.ex_int2\n" + 
-			"FROM\n" + 
-			"    tn_order_header head\n" + 
-			"WHERE\n" + 
-			"    head.del_status=0 AND\n" + 
-			"    head.delivery_date BETWEEN :fromDate AND :toDate\n" + 
-			"    GROUP BY head.order_status",nativeQuery=true)
+			"        UUID() AS id,\n" + 
+			"        COUNT(head.order_status) order_status_cnt,\n" + 
+			"        head.order_status,\n" + 
+			"        CASE \n" + 
+			"            WHEN head.order_status=0 THEN 'Park Order'       \n" + 
+			"            WHEN head.order_status=1 THEN 'Shop Confirmation Pending'        \n" + 
+			"            WHEN head.order_status=2 THEN 'Accept'         \n" + 
+			"            WHEN head.order_status=3 THEN 'Processing'          \n" + 
+			"            WHEN head.order_status=4 THEN 'Delivery Pending'           \n" + 
+			"            WHEN head.order_status=5 THEN 'Delivered'            \n" + 
+			"            WHEN head.order_status=6 THEN 'Rejected by Shop'             \n" + 
+			"            WHEN head.order_status=7 THEN 'Return Order'             \n" + 
+			"            WHEN head.order_status=8 THEN 'Cancelled Order'            \n" + 
+			"        ELSE  'Online Payment Pending'            \n" + 
+			"        END AS status_name,\n" + 
+			"        CONCAT(fr.fr_name,\"-\",fr.fr_code) AS fr_name,        \n" + 
+			"        head.ex_int1,\n" + 
+			"        head.ex_int2,\n" + 
+			"        head.ex_int3,\n" + 
+			"        head.ex_float1,\n" + 
+			"        head.ex_float2,\n" + 
+			"        head.ex_float3,\n" + 
+			"        head.ex_var1,\n" + 
+			"        head.ex_var2,\n" + 
+			"        head.ex_var3\n" + 
+			"    FROM\n" + 
+			"        tn_order_header head,\n" + 
+			"        m_franchisee fr\n" + 
+			"    WHERE\n" + 
+			"        head.del_status=0 \n" + 
+			"        AND     head.delivery_date BETWEEN :fromDate AND :toDate   \n" + 
+			"        AND 	head.fr_id=fr.fr_id\n" + 
+			"    GROUP BY\n" + 
+			"        head.order_status",nativeQuery=true)
 	List<GetDashPieStatusCnt> getDashStatusCnt(@Param("fromDate") String fromDate, @Param("toDate") String toDate);
+
+
+	@Query(value="SELECT \n" + 
+			"    UUID() AS id,\n" + 
+			"    COUNT(head.order_status) AS order_status_cnt, \n" + 
+			"    CASE \n" + 
+			"			             WHEN head.order_status=0 THEN 'Park Order'     \n" + 
+			"			             WHEN head.order_status=1 THEN 'Shop Confirmation Pending'      \n" + 
+			"			             WHEN head.order_status=2 THEN 'Accept'       \n" + 
+			"			             WHEN head.order_status=3 THEN 'Processing'        \n" + 
+			"			             WHEN head.order_status=4 THEN 'Delivery Pending'         \n" + 
+			"			             WHEN head.order_status=5 THEN 'Delivered'          \n" + 
+			"			             WHEN head.order_status=6 THEN 'Rejected by Shop'           \n" + 
+			"			             WHEN head.order_status=7 THEN 'Return Order'           \n" + 
+			"			             WHEN head.order_status=8 THEN 'Cancelled Order'          \n" + 
+			"			         ELSE  'Online Payment Pending'          \n" + 
+			"			       END AS status_name,\n" + 
+			"    head.order_status, \n" + 
+			"    fr.fr_id AS ex_int1,\n" + 
+			"    CONCAT(fr.fr_name,\"-\",fr.fr_code) AS fr_name,\n" + 
+			"    0 AS ex_int2,\n" + 
+			"    0 AS ex_int3,\n" + 
+			"    SUM(head.total_amt) AS ex_float1,\n" + 
+			"    0 AS  ex_float2,\n" + 
+			"    0 AS  ex_float3,\n" + 
+			"    'na' AS ex_var1,\n" + 
+			"    'na' AS ex_var2,\n" + 
+			"    'na' AS ex_var3,\n" + 
+			"    'na' AS from_date,\n" + 
+			"    'na' AS to_date\n" + 
+			"FROM \n" + 
+			"    tn_order_header head,\n" + 
+			"    m_franchisee fr\n" + 
+			"WHERE \n" + 
+			"    head.del_status=0 AND\n" + 
+			"    head.order_status=:status AND\n" + 
+			"    head.fr_id = fr.fr_id AND\n" + 
+			"    fr.del_status=0 AND \n" + 
+			"    head.delivery_date BETWEEN :fromDate AND :toDate\n" + 
+			"    \n" + 
+			"GROUP BY \n" + 
+			"    head.fr_id",nativeQuery=true)
+	List<GetDashPieStatusCnt> getStatusCntGrpFr(@Param("fromDate") String fromDate, @Param("toDate") String toDate, @Param("status") String status);
+
 }
+
+
+//SELECT\n" + 
+//"	UUID() AS id,\n" + 
+//"    COUNT(order_status) order_status_cnt,\n" + 
+//"    head.order_status,\n" + 
+//"    CASE WHEN head.order_status=0 THEN 'Park Order'\n" + 
+//"      WHEN head.order_status=1 THEN 'Shop Confirmation Pending'\n" + 
+//"       WHEN head.order_status=2 THEN 'Accept'\n" + 
+//"        WHEN head.order_status=3 THEN 'Processing'\n" + 
+//"         WHEN head.order_status=4 THEN 'Delivery Pending'\n" + 
+//"          WHEN head.order_status=5 THEN 'Delivered'\n" + 
+//"           WHEN head.order_status=6 THEN 'Rejected by Shop'\n" + 
+//"            WHEN head.order_status=7 THEN 'Return Order'\n" + 
+//"          	 WHEN head.order_status=8 THEN 'Cancelled Order'\n" + 
+//"           ELSE  'Online Payment Pending'\n" + 
+//"           END AS status_name,\n" + 
+//"    head.ex_int1,\n" + 
+//"    head.ex_int2\n" + 
+//"FROM\n" + 
+//"    tn_order_header head\n" + 
+//"WHERE\n" + 
+//"    head.del_status=0 AND\n" + 
+//"    head.delivery_date BETWEEN :fromDate AND :toDate\n" + 
+//"    GROUP BY head.order_status
+
+
