942723e1d3af88d5312c4205fc5c74104fe59265
Aaryatech <code@atsinfosoft.com>
2020-08-27 10:01:45 +0530

    Anmol

--- src/main/java/com/ats/ckweb/repository/CustomerRepo.java
+++ src/main/java/com/ats/ckweb/repository/CustomerRepo.java
@@ -3,6 +3,8 @@ package com.ats.ckweb.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ats.ckweb.model.Customer;
@@ -12,5 +14,12 @@ public interface CustomerRepo extends JpaRepository<Customer, Integer> {
 
 	List<Customer> findByDelStatusAndCompIdOrderByCustIdDesc(int del, int compId);
 	List<Customer> findByDelStatusAndCustId(int del, int custId);
+	
+	@Query(value="select c.* from m_customer c, tn_order_header h where h.del_status=0 AND h.cust_id=c.cust_id AND h.order_id=:orderId",nativeQuery=true)
+	Customer getCustomerByOrderId(@Param("orderId") int orderId);
+	
+	@Query(value="select c.* from m_customer c, t_grievences g, tn_order_header h where h.del_status=0 AND h.cust_id=c.cust_id AND h.order_id=g.order_id AND g.grieve_id=:grievId",nativeQuery=true)
+	Customer getCustomerByGrievId(@Param("grievId") int grievId);
+	
 	
 }
